e1e397130ceee591b0e5ec9a2133447d0b59058a
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-01-25 17:54:12 -0500

    react tankstack table

---
+++ src/components/tabla.jsx
@@ -0,0 +1,54 @@
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+function TablaEdit({ values, columns }) {
+  const table = useReactTable({
+    data: values,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+  return (
+    <div>
+      <table className="border-collapse border-spacing-0 w-full">
+        <thead className="bg-nepal-500">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className="py-4 text-nepal-50 font-WorkSansblod"
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className="py-4 text-center border-t border-black font-WorkSansregular"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TablaEdit;
--- src/pages/editar.jsx
+++ src/pages/editar.jsx
@@ -1,10 +1,48 @@
+import TablaEdit from "../components/tabla";
+import useGet from "../hooks/useGet";
+import { createColumnHelper } from "@tanstack/react-table";
 function Editar() {
+  const { values } = useGet("menu");
+  const columnHelper = createColumnHelper();
+  const columns = [
+    columnHelper.accessor((row) => row.img, {
+      id: "img",
+      cell: (info) => (
+        <div className="w-full flex justify-center">
+          <img
+            src={`http://localhost:3000/${info.getValue()}.webp`}
+            alt={info.getValue()}
+            className="w-16"
+          />
+        </div>
+      ),
+      header: () => "Imagen",
+    }),
+    columnHelper.accessor("nombre", {
+      header: () => "Nombre",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("precio", {
+      header: () => "Precio",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("vendidos", {
+      header: () => "Vendidos",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("id", {
+      header: () => "Editar/Eliminar",
+      cell: (info) => (
+        <div>
+          <button>Editar</button>
+          <button>Eliminar</button>
+        </div>
+      ),
+    }),
+  ];
   return (
-    <main>
-      <form>
-        <label>Editar Menu</label>
-        <input type="text" name="nombre" />
-      </form>
+    <main className="p-8">
+      <TablaEdit values={values} columns={columns} />
     </main>
   );
 }
