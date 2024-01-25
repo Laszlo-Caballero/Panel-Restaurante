abd67ebcc20a3afa371c9f3205be5168db1d9352
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-01-24 19:51:38 -0500

    page edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./components/navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Editar from "./pages/editar";
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/editar" element={<Editar />} />
         </Routes>
       </div>
     </>
---
+++ src/pages/editar.jsx
@@ -0,0 +1,12 @@
+function Editar() {
+  return (
+    <main>
+      <form>
+        <label>Editar Menu</label>
+        <input type="text" name="nombre" />
+      </form>
+    </main>
+  );
+}
+
+export default Editar;
