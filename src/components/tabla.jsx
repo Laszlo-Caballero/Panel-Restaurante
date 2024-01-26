945178018d75782c9dd0852d85a33722ac558d5a
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-01-25 21:58:12 -0500

    modal form

--- src/components/tabla.jsx
+++ src/components/tabla.jsx
@@ -3,6 +3,7 @@ import {
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import Modal from "./modal/modal";
 
 function TablaEdit({ values, columns }) {
   const table = useReactTable({
@@ -11,43 +12,52 @@ function TablaEdit({ values, columns }) {
     getCoreRowModel: getCoreRowModel(),
   });
   return (
-    <div>
-      <table className="border-collapse border-spacing-0 w-full">
-        <thead className="bg-nepal-500">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  className="py-4 text-nepal-50 font-WorkSansblod"
-                >
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td
-                  key={cell.id}
-                  className="py-4 text-center border-t border-black font-WorkSansregular"
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div>
+        <table className="border-collapse border-spacing-0 w-full shadow-2xl">
+          <thead className="bg-nepal-500">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    className="py-4 text-nepal-50 font-WorkSansblod"
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    key={cell.id}
+                    className="py-4 text-center border-t border-black font-WorkSansregular"
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Modal className="w-[60em] h-[50em] bg-nepal-900 rounded-xl p-8">
+        <form className="flex w-full h-full flex-col">
+          <h1 className="font-WorkSansblod text-xl text-nepal-100 w-full">
+            Añandir Producto
+          </h1>
+        </form>
+      </Modal>
+    </>
   );
 }
 
