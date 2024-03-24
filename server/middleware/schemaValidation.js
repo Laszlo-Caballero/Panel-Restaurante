e3f19e1ae0ea9c8d0611da646d90a93b7f5ba1fc
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-03-24 03:52:36 -0500

    rename folder schema

--- server/middleware/schemaValidation.js
+++ server/middleware/schemaValidation.js
@@ -1,4 +1,4 @@
-import { menuSchema } from "../utils/validation.js";
+import { menuSchema, userSchema, loginSchema } from "../utils/validation.js";
 export const ValidationMenu = (req, res, next) => {
   try {
     const { jsonData } = req.body;
@@ -23,6 +23,30 @@ export const ValidationUpdate = (req, res, next) => {
     next();
   } catch (error) {
     console.log(error);
+    return res.status(400).json({
+      error,
+    });
+  }
+};
+
+export const ValidationUser = (req, res, next) => {
+  const { nombre, email, contraseña } = req.body;
+  try {
+    userSchema.parse({ nombre, email, contraseña });
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      error,
+    });
+  }
+};
+
+export const ValidationLogin = (req, res, next) => {
+  const { email, contraseña } = req.body;
+  try {
+    loginSchema.parse({ email, contraseña });
+    next();
+  } catch (error) {
     return res.status(400).json({
       error,
     });
