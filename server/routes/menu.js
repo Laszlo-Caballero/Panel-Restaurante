3d2e301f418eb4ad5609fb39976c0cae90a111bb
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-03-24 01:30:10 -0500

    middlewares add

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -6,11 +6,25 @@ import {
   Food,
   Menu,
 } from "../controllers/menu/Menu.js";
+import {
+  ValidationMenu,
+  ValidationUpdate,
+} from "../middlewares/schemaValidation.js";
 
 const router = express.Router();
 
-router.post("/insertar", subirArchivo.single("file"), EnviarComida);
-router.put("/actualizar", subirArchivo.single("file"), ActualizarComida);
+router.post(
+  "/insertar",
+  subirArchivo.single("file"),
+  ValidationMenu,
+  EnviarComida
+);
+router.put(
+  "/actualizar",
+  subirArchivo.single("file"),
+  ValidationUpdate,
+  ActualizarComida
+);
 router.get("/", Menu);
 router.get("/comida/:id", Food);
 
