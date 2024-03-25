62ac1e9134005740d23bb8c11ded09db3303be43
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-03-25 01:49:23 -0500

    fix cors and cookies

--- server/index.js
+++ server/index.js
@@ -4,12 +4,19 @@ import routesM from "./routes/menu.js";
 import routesU from "./routes/auth.js";
 import bodyParser from "body-parser";
 import EliminarOtros from "./utils/eliminar.js";
+import cookieParser from "cookie-parser";
 EliminarOtros();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use("/menu", routesM);
 app.use("/users", routesU);
 app.use(express.static("storage"));
