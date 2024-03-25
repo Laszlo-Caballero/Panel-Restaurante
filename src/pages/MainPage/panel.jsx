afa16f7d23dd70cac6dd5df555808cafd97965b5
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-03-25 01:50:07 -0500

    ProtectedRoute

--- src/pages/MainPage/panel.jsx
+++ src/pages/MainPage/panel.jsx
@@ -5,16 +5,19 @@ import Home from "../home";
 import Editar from "../editar";
 import Catalogo from "../catalogo";
 import PanelComida from "../comida";
+import ProtecterRoute from "../../components/ProtecterRoute";
 export default function Panel() {
   return (
     <>
       <NavBar />
       <main className="w-5/6">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/editar" element={<Editar />} />
-          <Route path="/catalogo" element={<Catalogo />} />
-          <Route path="/menu/:id" element={<PanelComida />} />
+          <Route element={<ProtecterRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/editar" element={<Editar />} />
+            <Route path="/catalogo" element={<Catalogo />} />
+            <Route path="/menu/:id" element={<PanelComida />} />
+          </Route>
         </Routes>
       </main>
     </>
