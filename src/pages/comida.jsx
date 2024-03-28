f8d2256513110a22c96ac316dc3530e69d9d2d8b
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-03-28 01:17:36 -0500

    add page foods

--- src/pages/comida.jsx
+++ src/pages/comida.jsx
@@ -3,9 +3,34 @@ import useGet from "../hooks/useGet";
 
 function PanelComida() {
   const { id } = useParams();
-  const { values } = useGet(`menu/comida/${id}`);
+  const values = useGet(`menu/comida/${id}`)[0];
   console.log(values);
-  return <div>as</div>;
+  return (
+    <article className="p-8 bg-nepal-50">
+      <header>
+        <h2 className="font-WorkSansmedium text-2xl py-10 pl-4 border-b border-nepal-800">
+          Detalles del Producto
+        </h2>
+      </header>
+      <section className="pt-4 flex">
+        <div className="w-64 relative pl-4">
+          <img
+            src={values && `http://localhost:3000/${values.img}.webp`}
+            alt="Producto-Imagen"
+          />
+          <h3 className="font-WorkSansblod text-4xl z-2 absolute bottom-0 text-nepal-50 uppercase">
+            {values && values.nombre}
+          </h3>
+        </div>
+        <div>
+          <h4>Informacion del Producto</h4>
+          <p>
+            Precio <span>${values && values.precio}</span>
+          </p>
+        </div>
+      </section>
+    </article>
+  );
 }
 
 export default PanelComida;
