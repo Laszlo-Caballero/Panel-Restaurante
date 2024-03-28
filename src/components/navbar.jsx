e979da04b8491546c5c8bdc726b52dc048966303
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-03-28 02:48:20 -0500

    add Links navBar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,29 +1,29 @@
 import Logo from "./../images/logo.jpg";
-import { Box, Content } from "./Details";
+import { Box, Content, Links } from "./Details";
 import Dash from "./../images/dash.svg?react";
 import Menu from "./../images/box.svg?react";
 import Table from "./../images/tables.svg?react";
-import { Link } from "react-router-dom";
+import CreateIcon from "@mui/icons-material/Create";
 import AssignmentIcon from "@mui/icons-material/Assignment";
-
+import TableBarIcon from "@mui/icons-material/TableBar";
 function NavBar() {
   return (
     <nav className="w-1/5 border-2 min-h-screen relative bg-white shadow-nav">
       <div className="fixed w-1/6">
         <img src={Logo} className="w-[95%]" alt="logo" />
         <div className="h-auto flex flex-col items-center">
-          <Link
-            className="py-4 flex w-full px-4 items-center gap-3 cursor-pointer"
-            to="/panel"
-          >
-            <Dash /> dashboard
-          </Link>
-          <Link
-            className="py-4 flex w-full px-3 items-center gap-3 cursor-pointer"
-            to="/panel/Ordenes"
-          >
-            <AssignmentIcon /> Ordenes
-          </Link>
+          <Links icon={<Dash />} path="/panel">
+            DashBoard
+          </Links>
+          <Links icon={<TableBarIcon />} path="/panel/Mesas">
+            Ver Mesas
+          </Links>
+          <Links icon={<CreateIcon />} path="/panel/Pedir">
+            Pedir Comida
+          </Links>
+          <Links icon={<AssignmentIcon />} path="/panel/Ordenes">
+            Ordenes
+          </Links>
           <Box icon={<Menu />} title="Menu">
             <Content path="/panel/catalogo">Catalogo</Content>
             <Content path="/panel/editar">Editar Menu</Content>
